78701ccca6e4c912150dca94b87d51b1fc7e114e
Mich221205 <michelxquiros@gmail.com>
2025-10-04 17:25:06 -0600

    Add entity classes for core modules
    Introduced entity classes for Academico, Matricula, Notificaciones, OfertaAcademica, Pagos, Reportes, Seguridad, and UsuariosRoles modules. These classes define the main data structures for each module, supporting future business logic and data access implementations. Also updated project files to remove explicit Entities folder includes.

---
+++ PV_NA_Academico/Entities/HistorialAcademico.cs
@@ -0,0 +1,12 @@
+﻿namespace PV_NA_Academico.Entities
+{
+    public class HistorialAcademico
+    {
+        public string TipoIdentificacion { get; set; }
+        public string Identificacion { get; set; }
+        public string CodigoCurso { get; set; }
+        public string NombreCurso { get; set; }
+        public decimal Promedio { get; set; }
+        public string Periodo { get; set; }
+    }
+}
---
+++ PV_NA_Matricula/Entities/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿namespace PV_NA_Matricula.Entities
+{
+    public class Estudiante
+    {
+        public int ID_Estudiante { get; set; }
+        public string Identificacion { get; set; }
+        public string Tipo_Identificacion { get; set; }
+        public string Email { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
