f11afaa84725fdd295d32a8621419a8f094e7e81
Saul312714 <223607531+Saul312714@users.noreply.github.com>
2025-10-08 16:35:07 -0600

    Restaurar Entities y carpeta BD de OfertaAcademica desde commit ad318f0

---
+++ PV_NA_OfertaAcademica/Entities/Profesor.cs
@@ -0,0 +1,13 @@
+﻿namespace PV_NA_OfertaAcademica.Entities
+{
+    public class Profesor
+    {
+        public int ID_Profesor { get; set; }
+        public string Identificacion { get; set; } = string.Empty;
+		public string Tipo_Identificacion { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime Fecha_Nacimiento { get; set; }
+    }
+}
+
